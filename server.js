7459e7e51b9128dd2b8f62a0053a0cb460db4676
maarjaryytel <maarja@ryytel.eu>
2020-09-09 10:51:27 +0300

    kangelased

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 //nõuame seda supereheroes paketti,mida tõmbasime
-let superheroes = require("superheroes");
-
+//let superheroes = require("superheroes");
 //see on omadus ja tema meetod
 //console.log(superheroes.all);
-
 //loome uue muutuja randomhero ja proovime console.logiga kuvada
-console.log(superheroes.random());
-superheroes.all.forEach(hero => {
-    console.log(hero);
-});
+//console.log(superheroes.random());
+//foraech t;;tab ainult massiiviga
+//superheroes.all.forEach(hero => {
+   // console.log(hero);
+   
+//});
+
+const supervillains = require("supervillains");
+//console.log(supervillains.all);
+//console.log(supervillains.random());
+const supereheroes = require("superheroes");
+console.log(supervillains.random());
+
+supervillains.all.forEach(supervillain = > {
+    console.log(supervillains.random());
+})
+
+console.log(ˋ${superheroes.random()} fights ${supervillains.random()}ˋ);
+
+
+
+
